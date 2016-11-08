10f169c1f87af36246a02f9201563dad1038c562
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-11-08 00:14:51 -0200

    First commit

---
+++ src/model/cidade/CidadeDAO.java
@@ -0,0 +1,35 @@
+package model.cidade;
+
+import model.DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CidadeDAO extends DAO {
+    public CidadeDAO() {
+        super();
+    }
+
+    public List<Cidade> getAll() {
+        ArrayList<Cidade> cidades = new ArrayList<>();
+        try {
+
+            Connection connection = getConexao();
+            Statement statement = connection.createStatement();
+
+            String query = "SELECT * FROM cidades ORDER BY nome ASC";
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                cidades.add(new Cidade(rs.getInt("id"), rs.getString("nome"), rs.getBoolean("temAeroporto"), rs.getInt("numeroDiasIdeal")));
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return cidades;
+    }
+}
